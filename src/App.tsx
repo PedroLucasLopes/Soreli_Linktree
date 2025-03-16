c96410b3246668ecbe76d003428cf1f38cdd5ba9
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2025-03-16 12:09:35 -0300

    fix positions

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full md:max-w-10/12 md:mt-3 xl:mt-0 xl:max-w-7/12 md:top-30 xl:top-3/14 z-50">
         <Header />
       </div>
-      <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-[#D47831] via-[#D47831] via-70% to-[#ffffff00] flex flex-col items-center justify-center z-10 md:bg-gradient-to-t md:from-transparent md:via-transparent">
+      <div className="absolute bottom-0 w-full h-2/3 bg-gradient-to-t from-[#D47831] via-[#D47831] via-70% to-[#ffffff00] flex flex-col items-center justify-center z-10 md:bg-gradient-to-t md:from-transparent md:via-transparent">
         <MainContent />
         <img
           src={ico}
