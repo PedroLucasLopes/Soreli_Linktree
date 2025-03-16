a30ed21272e7e3cab111db86df7dbfca3a6249fd
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2025-03-16 12:34:05 -0300

    fix

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -19,10 +19,10 @@ const buttons = [
 
 const MainContent = () => {
   return (
-    <div className="flex xl:absolute xl:bottom-5 w-full flex-col gap-3 justify-center items-center xl:flex-row">
+    <div className="flex xl:absolute xl:bottom-8 w-full flex-col gap-3 justify-center items-center xl:flex-row">
       {buttons.map((button, i) => (
         <button
-          className={`btn bg-[#ededed] w-3/4 max-w-96 h-12 sm:h-14 md:h-16 xl:h-18 2xl:h-20 rounded-3xl text-[#706859] font-bold shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-[#fff] transition-all cursor-pointer md:text-2xl ${
+          className={`btn bg-[#ededed] w-3/4 max-w-96 h-12 sm:h-14 md:h-16 xl:h-16 2xl:h-20 rounded-3xl text-[#706859] font-bold shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-[#fff] transition-all cursor-pointer md:text-2xl ${
             i === 1 ? "xl:mr-25" : ""
           }`}
           key={i}
