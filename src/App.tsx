7cf4483d957b8654c9b1110cf72e67513d355fce
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2025-03-16 12:00:58 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ico from "./assets/ico.png";
 function App() {
   return (
     <div className="relative h-screen flex flex-col">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full md:max-w-10/12 md:mt-3 xl:mt-0 xl:max-w-7/12 md:top-30 xl:top-3/14 z-50">
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full md:max-w-10/12 md:mt-3 xl:mt-0 xl:max-w-7/12 md:top-30 xl:top-3/14 z-50">
         <Header />
       </div>
       <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-[#D47831] via-[#D47831] via-70% to-[#ffffff00] flex flex-col items-center justify-center z-10 md:bg-gradient-to-t md:from-transparent md:via-transparent">
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -22,7 +22,7 @@ const MainContent = () => {
     <div className="w-full h-full flex flex-col gap-3 justify-center items-center xl:flex-row xl:mt-80">
       {buttons.map((button, i) => (
         <button
-          className={`btn bg-[#ededed] w-full max-w-96 h-12 sm:h-14 md:h-16 xl:h-18 2xl:h-20 rounded-3xl text-[#706859] font-bold shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-[#fff] transition-all cursor-pointer md:text-2xl ${
+          className={`btn bg-[#ededed] w-2/3 max-w-96 h-12 sm:h-14 md:h-16 xl:h-18 2xl:h-20 rounded-3xl text-[#706859] font-bold shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-[#fff] transition-all cursor-pointer md:text-2xl ${
             i === 1 ? "xl:mr-32" : ""
           }`}
           key={i}
